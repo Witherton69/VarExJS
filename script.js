ed841cfdbc65b0fbe86dc73b0a01e76bf694a93a
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-02-10 13:06:29 -0600

    var ex

---
+++ script.js
@@ -0,0 +1,33 @@
+// 1
+// const
+
+// 2
+let num = 20;
+console.log(num); // 20
+
+// 3
+num = 15;
+console.log(num); // 15
+
+// 4
+num += 7;
+// num = num + 7;
+console.log(num); // 22
+
+// 5
+num--;
+console.log(num); // 21
+
+// 6
+num = "This is not a number";
+console.log(num); // This is not a number
+
+// 7
+let blank;
+
+// 8
+console.log(blank); // undefined
+
+// 9
+blank = null;
+console.log(blank); // null
